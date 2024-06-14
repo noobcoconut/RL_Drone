f0e9ce059e72a78ae54784a74d270747cf066ed2
noobcoconut <jerryterryharry@gmail.com>
2024-06-14 15:41:36 +0900

    updated test

--- sum_rew.py
+++ sum_rew.py
@@ -5,26 +5,79 @@ import matplotlib.pyplot as plt
 
 rew_list = []
 # Specify the path to the JSON file
-file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\p-0\\Check1'
-# file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\git_repos\\RL_Drone-1\\Check22b'
-for i in range(1,9):
+# file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\p-0\\Check1'
+file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\git_repos\\RL_Drone-1\\Check0'
+for i in range(1,10):
     # file_name = f"rew-102045-{i}.json" #02
     # file_name = f"rew-121802-{i}.json" #22b
-    file_name = f"rew-101525-{i}.json" #01
+    file_name = f"rew-092215-{i}.json"
+    file_path = os.path.join(file_dir, file_name)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        data.append(-9.2)
+        rew_list.extend(data)
+
+
+
+file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\git_repos\\RL_Drone-1\\Check1'
+for i in range(1,10):
+    # file_name = f"rew-102045-{i}.json" #02
+    # file_name = f"rew-121802-{i}.json" #22b
+    file_name = f"rew-101525-{i}.json"
+    file_path = os.path.join(file_dir, file_name)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        data.append(-5.3)
+        rew_list.extend(data)
+
+
+
+
+
+file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\git_repos\\RL_Drone-1\\Check2b'
+for i in range(0,10):
+    # file_name = f"rew-102045-{i}.json" #02
+    # file_name = f"rew-121802-{i}.json" #22b
+    file_name = f"rew-130158-{i}.json"
     file_path = os.path.join(file_dir, file_name)
     with open(file_path, 'r') as file:
         data = json.load(file)
         # data.append(-9)
         rew_list.extend(data)
 
-file_name = f"rew-101704-1.json" #01
-file_path = os.path.join(file_dir, file_name)
-with open(file_path, 'r') as file:
-    data = json.load(file)
-    # data.append(-9)
-    rew_list.extend(data)
 
-print(np.array(rew_list).mean())
+
+
+
+file_dir = 'C:\\Users\\jerry\\Desktop\\hly\\2024-1\\Rein\\git_repos\\RL_Drone-1\\Check3'
+for i in range(0,7):
+    # file_name = f"rew-102045-{i}.json" #02
+    # file_name = f"rew-121802-{i}.json" #22b
+    file_name = f"rew-131219-{i}.json"
+    file_path = os.path.join(file_dir, file_name)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        # data.append(-9)
+        rew_list.extend(data)
+
+for i in range(0,3):
+    # file_name = f"rew-102045-{i}.json" #02
+    # file_name = f"rew-121802-{i}.json" #22b
+    file_name = f"rew-131359-{i}.json"
+    file_path = os.path.join(file_dir, file_name)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        # data.append(-9)
+        rew_list.extend(data)
+
+# file_name = f"rew-101704-1.json" #01
+# file_path = os.path.join(file_dir, file_name)
+# with open(file_path, 'r') as file:
+#     data = json.load(file)
+#     # data.append(-9)
+#     rew_list.extend(data)
+
+# print(np.array(rew_list).mean())
 
 # rew_list = []
 
--- test.py
+++ test.py
@@ -26,7 +26,8 @@ class Configurations:
     gae_lambda = 0.95
     actor_hidden_size = 256
     critic_hidden_size = 256
-    num_episodes = 3
+    num_episodes = 4
+    #!!!!!
     max_timestep = 600
     checkpoint_path = "D:\\unreal\\Check"
     
@@ -65,15 +66,79 @@ class Critic(nn.Module):
         x = self.bn2(F.relu(self.fc2(x)))
         return self.fc3(x)
 
+
+class Target:
+    def __init__(self,i):
+        # target_temp = np.random.rand(3)*140-70 # target can be inside of an object
+        # if target_temp[2] > -5:
+        #     #NED coord
+        #     target_temp[2] = 5
+        # self.world_pos = airsim.Vector3r(target_temp[0], target_temp[1], target_temp[2])
+        signX = i%2*2-1                 #-1 1 -1 1
+        signY = math.floor(i/2)*2-1     #-1 -1 1 1
+        self.world_pos = airsim.Vector3r(80 * signX, 80*signY, -10)
+        # acc_tmp = np.random.rand(3)*1.2 - 0.6 # +- 6.7m/ss max
+        acc_tmp = np.random.rand(3)*1.2 - 0.6 # +- ?m/ss max
+        #!!!!!!!!
+        self.accel = airsim.Vector3r(acc_tmp[0], acc_tmp[1], acc_tmp[2])
+        self.vel = airsim.Vector3r(0,0,0)
+        self.max_speed = 0
+        #!!!!!!!!
+        self.how_long = 0
+
+    def step(self):
+        is_onEdge = False
+        self.how_long -= 1
+        self.vel += self.accel
+        # normalize
+        spd = self.vel.get_length()
+        if spd > self.max_speed:
+            self.vel = self.vel/spd * self.max_speed
+
+        # update pos !!!!!
+        # self.world_pos += self.vel
+
+        if -80 > self.world_pos.x_val:
+            self.accel.x_val = 1.2
+            is_onEdge = True
+            self.how_long = 0
+        elif self.world_pos.x_val > 80:
+            self.accel.x_val = -1.2
+            is_onEdge = True
+            self.how_long = 0
+        if -80 > self.world_pos.y_val:
+            self.accel.y_val = 1.2
+            is_onEdge = True
+            self.how_long = 0
+        elif self.world_pos.y_val > 80:
+            self.accel.y_val = -1.2
+            is_onEdge = True
+            self.how_long = 0
+        if -70 > self.world_pos.z_val:
+            self.accel.z_val = 1.2
+            is_onEdge = True
+            self.how_long = 0
+        elif self.world_pos.z_val > 0:
+            self.world_pos.z_val = 0
+            self.how_long = 0
+
+
+        if self.how_long < 1 and not is_onEdge:
+            self.accel = airsim.Vector3r(np.random.rand()*1.2 - 0.6, np.random.rand()*1.2 - 0.6, np.random.rand()*1.2 - 0.6)
+            # !!!!!
+            self.how_long = np.random.randint(10,50)
+
+
 class Environment:
-    #!!!!!!! check if clock speed affects move duration
     def configure(self):
         self.steps_per_second = 5
         self.max_speed = 21
+        self.goal_distance = 8
+        # !!!!!!!!^8
         # in deg/.2s, 36 -> 180deg/s 
         self.max_yaw_rate = 36
-        self.target_distance = 8
-        self.tolerance = 2 
+        self.tolerance = 2
+        #!!!!!!!^2
         self.max_wind = 5
         self.wind_d_rate = 0.1
 
@@ -86,30 +151,29 @@ class Environment:
         self.action_mul = torch.tensor([4,2,1,]*4)
         self.speed_step = self.max_speed/3
         self.yaw_step = self.max_yaw_rate/3
+        # target
+        self.target = Target(0)
+        # wind
         self.wind_vec = airsim.Vector3r(np.random.rand()*10 - 5, np.random.rand()*10 - 5, np.random.rand()*10 - 5)
         self.half_windd = self.wind_d_rate/2
         wind_amp = self.wind_vec.get_length()
         if wind_amp > self.max_wind:
             self.wind_vec / wind_amp * self.max_wind
+        # time and enter data
         self.t_in = -1
         self.t_step = 0
         self.is_in = False
+        # dimensions
         self.action_dim = 3 + 3 + 3 + 3 #vx vy vz yaw
-        self.state_dim = 3+6+3+3+1 #target(in local) sensor wind me(veloxyz, avel-yaw)
+        self.state_dim = 3+30+3+3+1 #target(in local) sensor wind me(veloxyz, avel-yaw)
         # setup client cam
         camera_pose_l = airsim.Pose(airsim.Vector3r(0, 0, -.5), airsim.to_quaternion(0, 0, math.radians(-90))) #radians
         camera_pose_r = airsim.Pose(airsim.Vector3r(0, 0, -.5), airsim.to_quaternion(0, 0, math.radians(90))) #radians
         self.client.simSetCameraPose("front_left", camera_pose_l)
         self.client.simSetCameraPose("front_right", camera_pose_r)
     
-    def reset(self):
-        target_temp = np.random.rand(3)*160-80 # target can be inside of an object
-        if target_temp[2] > 0:
-            #NED coord
-            target_temp[2] = 0
-        # self.target_world = airsim.Vector3r(target_temp[0], target_temp[1], target_temp[2])
-        self.target_world = airsim.Vector3r(50,50,-10)
-        # print(f"target:{self.target_world}")
+    def reset(self,i):
+        self.target = Target(i)
         self.client.reset()
         self.client.enableApiControl(True)
         self.client.armDisarm(True)
@@ -124,17 +188,17 @@ class Environment:
         self.t_in = -1
         self.t_step = 0
         self.was_in = False
-        return self.get_state((0,0,-2,0))
-        # return self.get_state((0,0,0,0))
+        return self.get_state((0,0,-1,0))
 
     def get_state(self, last_vel):
         state = [] # target x, y ,z / closest front, left, right, back, bottom, top / wind x y z / prevmove vxyz yaw
 
         # target coord in local
-        local_target = self.target_world - self.client.simGetVehiclePose().position
+        local_target = self.target.world_pos - self.client.simGetVehiclePose().position
         world_ori = self.client.simGetVehiclePose().orientation.inverse()
+        world_ori_conj = world_ori.conjugate()
         local_target_q = local_target.to_Quaternionr()
-        local_target = world_ori * local_target_q * world_ori.conjugate()
+        local_target = world_ori * local_target_q * world_ori_conj
         state.append(local_target.x_val)
         state.append(local_target.y_val)
         state.append(local_target.z_val)
@@ -148,13 +212,23 @@ class Environment:
         airsim.ImageRequest("bottom_center", airsim.ImageType.DepthPerspective, True, False),
         airsim.ImageRequest("top_center", airsim.ImageType.DepthPerspective, True, False),])
 
+        # images = np.array([])
         for response in responses:
-            state.append( np.min(response.image_data_float) )
+            img = np.array(response.image_data_float).reshape((40,40))
+            #img size 40 x 40, divide by 9 22 9
+            state.append(np.min(img[:, :9]))
+            state.append(np.min(img[:9, 9:31]))
+            state.append(np.min(img[9:31, 9:31]))
+            state.append(np.min(img[31:, 9:31]))
+            state.append(np.min(img[:, 31:]))
 
-        # wind
-        state.append(self.wind_vec.x_val)
-        state.append(self.wind_vec.y_val)
-        state.append(self.wind_vec.z_val)
+
+        # wind in local
+        local_wind_q = self.wind_vec.to_Quaternionr()
+        local_wind = world_ori * local_wind_q * world_ori_conj
+        state.append(local_wind.x_val)
+        state.append(local_wind.y_val)
+        state.append(local_wind.z_val)
 
         # last actions in v xyz yaw
         state.extend(last_vel)
@@ -183,6 +257,8 @@ class Environment:
         self.client.simContinueForTime(self.step_duration)
         self.client.moveByVelocityBodyFrameAsync(vx, vy, vz, duration = self.step_duration + 0.1, yaw_mode= airsim.YawMode(is_rate = True, yaw_or_rate = vyaw))
         time.sleep(self.step_duration/3)
+        # update target
+        self.target.step()
         # update wind
         d_vec = airsim.Vector3r(np.random.rand()*self.wind_d_rate - self.half_windd, np.random.rand()*self.wind_d_rate - self.half_windd, np.random.rand()*self.wind_d_rate - self.half_windd)
         self.wind_vec += d_vec
@@ -200,10 +276,9 @@ class Environment:
             print(f"collided! step:{self.t_step}")
             done = True
         else:
-            inner = self.target_distance - self.tolerance
-            outter = self.target_distance + self.tolerance
-            #TODO optimize it
-            local_target_norot = self.target_world - self.client.simGetVehiclePose().position
+            inner = self.goal_distance - self.tolerance
+            outter = self.goal_distance + self.tolerance
+            local_target_norot = self.target.world_pos - self.client.simGetVehiclePose().position
             target_deviation = local_target_norot.get_length()
             if target_deviation > outter:
                 # a bit too far
@@ -216,9 +291,10 @@ class Environment:
                 if(not self.was_in):
                     # update last in time
                     self.t_in = self.t_step
-                    print(f"enterted! step: {self.t_in}")
+                    print(f"entered! step: {self.t_in}")
                     self.was_in = True
-                reward = np.log(self.t_step - self.t_in + np.e)+1
+                reward = np.log(self.t_step - self.t_in + np.e)+1.4
+                #!!!!!!
             else:
                 # too close
                 print(f"too close, step: {self.t_step}")
@@ -226,11 +302,12 @@ class Environment:
                 reward = -1/(target_deviation/inner + 0.5)
         
         my_pos = self.client.simGetVehiclePose().position
-        if my_pos.x_val < -100 or 100 < my_pos.x_val or my_pos.y_val < -100 or 100 < my_pos.y_val or my_pos.z_val < -100:
+        if my_pos.x_val < -100 or 100 < my_pos.x_val or my_pos.y_val < -100 or 100 < my_pos.y_val or my_pos.z_val < -85:
             # out of env, too high up
+            print(f"out of env step: {self.t_step}")
             done = True
         return self.get_state((vx, vy, vz, vyaw)), reward, done, None
-        # return self.get_state((0,0,0,0)), reward, done, None
+        
 
 
 class DummyEnv:
@@ -254,8 +331,10 @@ actor = Actor(input_dim, output_dim, configs.actor_hidden_size).to(device)
 
 # load checkpoint data
 print("loading check point")
-check_file = "Check-291843.pth"
-checkpoint = torch.load(os.path.join(configs.checkpoint_path, check_file))
+check_file = "Check-131219.pth"
+checkpoint = torch.load(os.path.join("D:\\unreal\\git_repos\\RL_Drone\\Check", check_file))
+
+# checkpoint = torch.load(os.path.join(configs.checkpoint_path, check_file))
 actor.load_state_dict(checkpoint['Actor_state_dict'])
 # critic.load_state_dict(checkpoint['Critic_state_dict'])
 # actor_optimizer.load_state_dict(checkpoint['optimizerA_state_dict'])
@@ -268,15 +347,18 @@ actor.eval()
 rewards_hist = []
 
 for episode in range(configs.num_episodes):
-    state = env.reset()
+    state = env.reset(episode)
     rewards = []
 
     for _ in range(configs.max_timestep):  # Upper bound on episode length
         state_tensor = to_gpu_tensor(state)
         with torch.no_grad():
           action_prob = actor(state_tensor).to('cpu')
-        action = [np.random.choice([0, 1], p=[1-p.item(), p.item()]) for p in action_prob.detach()]
-        # action = [round(p.item()) for p in action_prob.detach()]
+        probs = F.hardtanh(action_prob, 0.15, 0.85)
+        probs = (probs - 0.15) * 1.4285 # mapping range into  [0, 1]
+
+        action = [np.random.choice([0, 1], p=[1-p.item(), p.item()]) for p in probs.detach()]
+        # action = [0,1,1, 0,1,1, 0,1,1, 0,0,0]
         action = torch.Tensor(action)
         next_state, reward, done, _ = env.step(action)
         # sigmoid -> as prob foreach -> log (mul_product of (each prob))
